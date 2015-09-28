6ca1c3e3111c8383ed56960d1b22d406d827085d
batmanimal <thejenniekim@gmail.com>
2015-09-28 11:21:50 -0700

    Add solution to stock prices question

--- stock_prices_yesterday/stock_prices_yesterday.js
+++ stock_prices_yesterday/stock_prices_yesterday.js
@@ -9,3 +9,34 @@ Write an efficient function that takes stock_prices_yesterday and returns the be
 
 No "shorting"—you must buy before you sell. You may not buy and sell in the same time step (at least 1 minute must pass).
 */
+
+
+
+var getMaxProfit = function(arr) {
+  var max = 0;
+  var adjustment = 0;
+  var diff;
+  // iterate over input array
+  for (var i = 1; i < arr.length; i++) {
+    // get item - first item
+    diff = arr[i] - arr[0];
+    // if negative, take the abs val and set to adjustment
+    if (diff < 0 && Math.abs(diff) > adjustment) {
+      adjustment = Math.abs(diff);
+    } else {
+      // if not neg, add adjustment 
+      diff += adjustment;
+      // if >= max, set the diff to max 
+      if (diff >= max) {
+        max = diff;
+      }
+    } 
+  }
+  return max;
+};
+
+
+// TESTING 
+
+var stock_prices_yesterday = [ 4, 9, 10, 3, 7, 1, 9, 7, 4, 15 ];
+// console.log(getMaxProfit(stock_prices_yesterday));
